cc77724214ce3f37f6c1646ca67da2c886395001
jaanli <jaan.li@jaan.li>
2024-10-15 15:27:22 -0400

    update with active medical groups

---
+++ results/search_active_medical_groups.sh
@@ -0,0 +1,23 @@
+duckdb -c "COPY (
+    SELECT name, current_status, current_alternative_legal_name, incorporation_date, 
+           registered_address_street_address, registered_address_region
+    FROM '~/data/opencorporates/us_*/companies.parquet'
+    WHERE name ILIKE '%medical group%'
+      AND (
+        name ILIKE '%surg%'
+        OR name ILIKE '%cardiol%'
+        OR name ILIKE '%oncolog%'
+        OR name ILIKE '%pediatr%'
+        OR name ILIKE '%neurol%'
+        OR name ILIKE '%ortho%'
+      )
+      AND current_status NOT ILIKE '%inactive%'
+      AND current_status NOT ILIKE '%dissolved%'
+      AND current_status NOT ILIKE '%terminated%'
+      AND current_status NOT ILIKE '%revoked%'
+      AND current_status NOT ILIKE '%converted%'
+      AND current_status NOT ILIKE '%forfeited%'
+      AND current_status NOT ILIKE '%cancelled%'
+      AND current_status NOT ILIKE '%suspended%'
+    LIMIT 10000
+  ) TO 'active_medical_groups.csv' (HEADER, DELIMITER ',');"
